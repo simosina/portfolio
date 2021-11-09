93eac4aae6a7828a32831d35200ce66d686607fc
Simone <simone.sinagra96@gmail.com>
2021-11-09 15:50:05 +0100

    Update ComingSoon page

--- src/components/comingSoon/ComingSoon.jsx
+++ src/components/comingSoon/ComingSoon.jsx
@@ -3,11 +3,12 @@ import "./comingSoon.scss";
 export default function ComingSoon() {
     return (
         <div className="comingSoon">
-            <img src="assets/logo-blue.svg" alt="" />
+            <img className="logo" src="assets/logo-blue.svg" alt="" />
             <h1>COMING SOON</h1>
+            <img className="rotate" src="assets/rotate.svg" alt="" />
             <p>
                 The website is currently under construction. It should be available in a responsive version shortly. 
-                In the meantime, you can visit it with your laptop or tablet.
+                In the meantime, you can visit it with your laptop or tablet or rotate your device.
             </p>
         </div>
     )
