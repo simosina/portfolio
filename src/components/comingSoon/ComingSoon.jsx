78434ab80aeafb769aae375663f10fd60be6abe8
Simone <simone.sinagra96@gmail.com>
2021-11-11 23:09:11 +0100

    First responsive version

--- src/components/comingSoon/ComingSoon.jsx
+++ src/components/comingSoon/ComingSoon.jsx
@@ -5,10 +5,10 @@ export default function ComingSoon() {
         <div className="comingSoon">
             <img className="logo" src="assets/logo-blue.svg" alt="" />
             <h1>COMING SOON</h1>
-            {/* <img className="rotate" src="assets/rotate.svg" alt="" /> */ }
+            <img className="rotate" src="assets/rotate.svg" alt="" />
             <p>
                 The mobile variant of the website is currently under construction. It should be available in a responsive version shortly. 
-                In the meantime, you can use your desktop.
+                In the meantime, you can rotate your phone or use your tablet or your personal computer.
             </p>
         </div>
     )
